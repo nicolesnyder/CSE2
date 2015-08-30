e5e5502a3fb8f345a884d36ce272d57a68b20eb0
nicolesnyder <nhs218@lehigh.edu>
2015-08-30 23:28:41 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,21 @@
+//Nicole Snyder
+//CSE2 hw01 Welcome Class
+
+public class WelcomeClass{ //define the class
+	public static void main(String [] args){ //add a main method
+// begin printing desired output
+
+
+System.out.println("  -----------"); 
+System.out.println("  | WELCOME |");
+System.out.println("  -----------"); 
+System.out.println("  ^  ^  ^  ^  ^  ^");  
+System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\"); 
+System.out.println("<-N--H--S--2--1--8->");
+System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+System.out.println("  v  v  v  v  v  v");  
+System.out.println("This summer I worked at an overnight camp as a camp counselor."); 
+System.out.println("This was my eleventh summer at Camp Wayne.");
+
+	}//closes main method
+}//closes class
---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+// Nicole Snyder
+// CSE2 lab01 Hello World
+
+public class HelloWorld{ //define the class
+	public static void main(String [] args){ //add a main method
+// begin printing desired output
+System.out.println("########################");
+System.out.println("########################");
+System.out.println("#                      #");
+System.out.println("#     Hello World 2015 #");
+System.out.println("#                      #");
+System.out.println("########################");
+System.out.println("########################");
+System.out.println("test");
+
+	} //ends main method
+} //ends class
